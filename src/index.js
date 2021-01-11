5b6c87a5920f9747f4ff70b04934f028a52786a0
Gary <garyarzumanyan@gmail.com>
2021-01-10 23:14:49 -0800

    Formated contents of weather displaying

--- src/index.js
+++ src/index.js
@@ -11,21 +11,19 @@ async function getWeather(city) {
     throw new Error("Location not found!");
   } catch (error) {
     console.log(error);
-    //handleError();
+    handleError();
   }
 }
 
 function processJSON(weatherJSON) {
   async function getObject(processJSON) {
     tempObject = await processJSON;
-
+    console.log(tempObject);
     const weatherObject = {};
     weatherObject.name = tempObject.name;
     weatherObject.country = tempObject.sys.country;
     weatherObject.description = tempObject.weather[0].description;
     weatherObject.temp = tempObject.main.temp;
-    weatherObject.maxTemp = tempObject.main.temp_max;
-    weatherObject.minTemp = tempObject.main.temp_min;
     weatherObject.feelsLike = tempObject.main.feels_like;
 
     return weatherObject;
@@ -36,15 +34,41 @@ function processJSON(weatherJSON) {
   return weatherObject;
 }
 
+function handleError() {
+  const weatherContainer = document.getElementById("weatherContainer");
+  const myDiv = document.createElement("div");
+  myDiv.className = "error";
+  myDiv.textContent = "Location not found!";
+  weatherContainer.innerHTML = ""; //clear current contents
+  weatherContainer.appendChild(myDiv);
+}
+
 function displayWeather(weatherObject) {
   const weatherContainer = document.getElementById("weatherContainer");
+  weatherContainer.innerHTML = ""; //clear contents
   weatherObject.then((weather) => {
     //waits until weatherObject is resolved THEN displays the div
     for (const myKey in weather) {
       const myDiv = document.createElement("div");
-      myDiv.className = weather.myKey;
-      myDiv.textContent = weather[myKey];
-      weatherContainer.appendChild(myDiv);
+      myDiv.className = myKey;
+      switch (myKey) {
+        case "name":
+          myDiv.textContent = `${weather.name}, ${weather.country}`;
+          break;
+        case "description":
+          myDiv.textContent =
+            weather[myKey].charAt(0).toUpperCase() + weather[myKey].slice(1);
+          break;
+        case "temp":
+          myDiv.textContent = Math.round(weather[myKey]) + "\u00B0F";
+          break;
+        case "feelsLike":
+          myDiv.textContent =
+            "Feels like: " + Math.round(weather[myKey]) + "\u00B0F";
+      }
+      if (myKey !== "country") {
+        weatherContainer.appendChild(myDiv);
+      }
     }
   });
 }
@@ -57,4 +81,13 @@ function getUserInput() {
   //console.log(myObject);
 }
 
+//event listeners for clicking on Find and pressing enter when in textbox
 document.getElementById("submit").addEventListener("click", getUserInput);
+document
+  .getElementById("userInput")
+  .addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      getUserInput();
+    }
+  });
